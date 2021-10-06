7de0308fe12e17b03734dedc456e5e3c7079e62b
Theotrgl <Justintheofilus@gmail.com>
2021-10-06 17:18:20 +0700

    Justin-L1CC-Lab Assignment

---
+++ Lab Assignment 1 Algo and Programming.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 18:13:02 2021
+
+@author: WINDOWS10
+"""
+
+print("Please choose what mathematical operation you would like to do!!(1.Additions 2.Subtraction 3.Multiplications 4.Divisions)") 
+x=input("Enter the number of your operation of choice:")
+
+if x=="1" or x=="2" or x=="3" or x=="4":
+     a=eval(input("Enter a number:"))
+     if int or float:
+        b=eval(input("Enter another number:"))
+        if x=="1" and (int or float):
+             print(eval("a+b"))
+        elif x=="2" and (int or float):
+            print(eval("a-b"))
+        elif x=="3" and (int or float):
+            print(eval("a*b"))
+        elif x=="4" and (int or float):
+            print(eval("a/b"))
+            
+
+                            
+             
